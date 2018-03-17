9912aec5fed51c5549961e6a1433f68c4b5af3c4
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-03-17 20:06:35 +0000

    Retrieving props passing through the component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const person = () => <p>I am a Person and I am {Math.floor(Math.random() * 30)} years old!</p>;
+const person = props => <p>I am {props.name} and I am {props.age} years old!</p>;
 
 export default person;
