deda278b0d4859d3ae714e4a0d17691936cb6ce6
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-03-17 20:18:29 +0000

    Accessing children props of a component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const person = props => <p>I am {props.name} and I am {props.age} years old!</p>;
+const person = props => (
+  <div>
+    <h3>I am {props.name} and I am {props.age} years old!</h3>
+    <p>{props.children}</p>
+  </div>
+);
 
 export default person;
