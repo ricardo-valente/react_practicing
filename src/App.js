86e71613e8e9fe4620e72b49d28beae394a911fa
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-03-17 19:34:40 +0000

    imported Person component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">Welcome to React</h1>
-    //     </header>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Ricardo Valente'));
+    return (
+      <div className="App">
+        <header>
+          <h1>Hi! I am a React App.</h1>
+          <p>coded by Ricardo Valente</p>
+          <Person />
+        </header>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Ricardo Valente'));
   }
 }
 
