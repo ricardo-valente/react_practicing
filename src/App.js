3e861bdfa8ad0309cd0706da565a12e49b990eb9
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-03-17 20:18:11 +0000

    included content inside a component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header>
           <h1>Hi! I am a React App.</h1>
           <p>coded by Ricardo Valente</p>
-          <Person name="Ricardo" age="31" />
+          <Person name="Ricardo" age="31">My interest: crossfit, ping pong & coding</Person>
           <Person name="João" age="25" />
           <Person name="Laurentino" age="28" />
         </header>
