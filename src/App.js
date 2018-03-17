57fcb96a2af430fa5b409e1225dbca088be6793c
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-03-17 19:02:30 +0000

    Understanding the Base Features & Syntax | Understanding JSX

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to React</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //   </div>
+    // );
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Ricardo Valente'));
   }
 }
 
