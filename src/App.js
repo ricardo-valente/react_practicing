2be517744e7a4d8aaa48ba9aa8fb133f34b3c130
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-05-27 17:03:20 +0100

    Removed the on going course code && created an input field with an onChange event handler that updates the state with its value length

--- src/App.js
+++ src/App.js
@@ -1,77 +1,24 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
 
 class App extends Component {
 
   state = {
-    persons: [
-      { id: 'fd62fd', name: 'Ricardo', age: '31' },
-      { id: '23sw2h', name: 'João', age: '25' },
-      { id: '965j6y', name: 'Laurentino', age: '28' }
-    ],
-    otherState: 'some other name',
-    showPersons: false
+    inputLength: 0
   }
 
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  }
-
-  deletePersonsHandler = (personIndex) => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
-
-  changeNameHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(person => {
-      return person.id === id
+  inputLengthHandler = event => {
+    this.setState({
+      inputLength: event.target.value.length
     })
-    const person = {
-      ...this.state.persons[personIndex]
-    }
-    person.name = event.target.value
-    const persons = [...this.state.persons]
-    persons[personIndex] = person
-    this.setState({ persons: persons })
   }
 
   render() {
-    const buttonStyle = {
-      padding: '10px 25px',
-      color: '#444',
-      border: '2px solid #444',
-      borderRadius: '5px'
-    }
-
-    let persons = null;
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonsHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              change={event => this.changeNameHandler(event, person.id)} />
-          })}
-        </div>
-      );
-    }
-
     return (
-      <div className="App">
-        <header>
-          <h1>Hi! I am a React App.</h1>
-          <p>coded by Ricardo Valente</p>
-          <button style={buttonStyle} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </header>
+      <div className="app">
+        <input type="text" onChange={this.inputLengthHandler} />
+        <p>{this.state.inputLength}</p>
       </div>
-    );
+    )
   }
 }
 
