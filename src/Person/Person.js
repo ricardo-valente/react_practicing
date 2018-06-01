e7201ce8aaf309268bbb2aea6e60700dd865fb7c
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-06-01 21:05:18 +0200

    Revert "Section #6 | #75 - Using Error Boundaries (React 16+)"

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,10 +2,6 @@ import React from 'react'
 import personStyles from './Person.css'
 
 const person = props => {
-  const rnd = Math.random()
-  if (rnd < 0.5) {
-    throw new Error('Something went wrong!')
-  }
   return (
     <div className={personStyles.person}>
       <h3 onClick={props.click}>I am {props.name} and I am {props.age} years old!</h3>
