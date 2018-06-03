bd8b27ce302e8bc20997e704c67267fa1bc0de22
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-06-03 21:52:50 +0100

    added a reference of the input element passing inputElement through the ref react’s special prop

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -16,6 +16,7 @@ class Person extends Component {
 
   componentDidMount() {
     console.log('[Person.js] init componentDidMount()')
+    this.inputElem.focus()
   }
 
   render() {
@@ -24,7 +25,11 @@ class Person extends Component {
       <Util>
         <h3 onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!</h3>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.change} value={this.props.name} />
+        <input
+          ref={(input) => this.inputElem = input}
+          type="text"
+          onChange={this.props.change}
+          value={this.props.name} />
       </Util>
     )
   }
