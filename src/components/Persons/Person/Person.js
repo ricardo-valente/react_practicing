db995032130c535b0eae0bc79a738c87c2d17cf3
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-06-17 18:54:26 +0100

    Implemented new react method to create ref’s

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,6 +8,7 @@ class Person extends Component {
   constructor(props) {
     super(props)
     console.log('[Person.js] init Constructor()', props)
+    this.inputElem = React.createRef()
   }
 
   componentWillMount() {
@@ -17,7 +18,7 @@ class Person extends Component {
   componentDidMount() {
     console.log('[Person.js] init componentDidMount()')
     if (this.props.index === 0) {
-      this.inputElem.focus()
+      this.inputElem.current.focus()
     }
   }
 
@@ -28,7 +29,7 @@ class Person extends Component {
         <h3 onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!</h3>
         <p>{this.props.children}</p>
         <input
-          ref={(input) => this.inputElem = input}
+          ref={this.inputElem}
           type="text"
           onChange={this.props.change}
           value={this.props.name} />
